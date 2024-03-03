7303b391523d6ea49f17c7696998acfe3f0df09e
Yoonyoung Heo <gjdbsdud101@naver.com>
2024-03-03 20:52:49 +0900

    [Feat] Page Router 구조 생성

---
+++ src/pages/stores/[id]/index.tsx
@@ -0,0 +1,13 @@
+import { useRouter } from "next/router"
+
+export default function StorePage() {
+    const router = useRouter();
+    const { id } = router.query;
+    return (
+        <div>
+            <h1>
+                Store Detail: {id}
+            </h1>
+        </div>
+    )
+}
