3286090555052e66519ea12c9c63a0ed2f02c52a
Yoonyoung Heo <gjdbsdud101@naver.com>
2024-03-31 16:45:19 +0900

    [Feat] 음식점 업종별 Marker Image 지정

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -3,10 +3,12 @@
 /*
 Kakao Map Docu
 https://apis.map.kakao.com/web/guide/
-마커 생성 Sample
-https://apis.map.kakao.com/web/sample/basicMarker/
 공공 데이터
 http://data.seoul.go.kr/dataList/OA-2741/S/1/datasetView.do
+마커 생성 Sample
+https://apis.map.kakao.com/web/sample/basicMarker/
+이미지 마커 생성 Sample
+https://apis.map.kakao.com/web/sample/basicMarkerImage/
 */
 
 import Script from "next/script";
@@ -39,6 +41,19 @@ export default function Map() {
 
       // 식당 데이터 마커 띄우기
       stores?.["DATA"]?.map((store) => {
+        var imageSrc = store?.bizcnd_code_nm
+            ? `/images/markers/${store?.bizcnd_code_nm}.png`
+            : "/images/markers/default.png", // 마커이미지의 주소입니다
+          imageSize = new window.kakao.maps.Size(40, 40), // 마커이미지의 크기입니다
+          imageOption = { offset: new window.kakao.maps.Point(27, 69) }; // 마커이미지의 옵션입니다. 마커의 좌표와 일치시킬 이미지 안에서의 좌표를 설정합니다.
+
+        // 마커의 이미지정보를 가지고 있는 마커이미지를 생성합니다
+        var markerImage = new window.kakao.maps.MarkerImage(
+          imageSrc,
+          imageSize,
+          imageOption
+        );
+
         // 마커가 표시될 위치입니다
         var markerPosition = new window.kakao.maps.LatLng(
           store?.y_dnts,
@@ -48,6 +63,7 @@ export default function Map() {
         // 마커를 생성합니다
         var marker = new window.kakao.maps.Marker({
           position: markerPosition,
+          image: markerImage
         });
 
         // 마커가 지도 위에 표시되도록 설정합니다
