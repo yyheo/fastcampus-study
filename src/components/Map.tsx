bca61735d1b537f8ab0204f75bcc98a85e5ff8a8
Yoonyoung Heo <gjdbsdud101@naver.com>
2024-03-31 17:03:57 +0900

    [Feat] Marker에 Mouseover시 CustomOverlay 띄우기

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -9,6 +9,12 @@ http://data.seoul.go.kr/dataList/OA-2741/S/1/datasetView.do
 https://apis.map.kakao.com/web/sample/basicMarker/
 이미지 마커 생성 Sample
 https://apis.map.kakao.com/web/sample/basicMarkerImage/
+커스텀 오버레이 생성하기
+https://apis.map.kakao.com/web/sample/customOverlay1/
+마커에 마우스 이벤트 등록하기
+https://apis.map.kakao.com/web/sample/addMarkerMouseEvent/
+마커에 클릭 이벤트 등록하기
+https://apis.map.kakao.com/web/sample/addMarkerClickEvent/
 */
 
 import Script from "next/script";
@@ -63,7 +69,7 @@ export default function Map() {
         // 마커를 생성합니다
         var marker = new window.kakao.maps.Marker({
           position: markerPosition,
-          image: markerImage
+          image: markerImage,
         });
 
         // 마커가 지도 위에 표시되도록 설정합니다
@@ -71,6 +77,32 @@ export default function Map() {
 
         // 아래 코드는 지도 위의 마커를 제거하는 코드입니다
         // marker.setMap(null);
+
+        // 마커 커서가 오버 되었을때 마커 위에 표시 될 커스텀 오버레이 생성
+
+        // 커스텀 오버레이에 표시할 내용입니다
+        // HTML 문자열 또는 Dom Element 입니다
+        var customOverlayContent = `<div class="info-window">${store?.upso_nm}</div>`;
+
+        // 커스텀 오버레이를 생성합니다
+        var customOverlay = new window.kakao.maps.CustomOverlay({
+          position: markerPosition,
+          content: customOverlayContent,
+          xAnchor: 0.6,
+          yAnchor: 0.91,
+        });
+
+        // 마커에 마우스오버 이벤트를 등록합니다
+        window.kakao.maps.event.addListener(marker, "mouseover", function () {
+          // 마커에 마우스오버 이벤트가 발생하면 커스텀 오버레이를 지도에 표시합니다
+          customOverlay.setMap(map);
+        });
+
+        // 마커에 마우스아웃 이벤트를 등록합니다
+        window.kakao.maps.event.addListener(marker, "mouseout", function () {
+          // 마커에 마우스아웃 이벤트가 발생하면 커스텀오버레이 제거
+          customOverlay.setMap(null);
+        });
       });
     });
   };
