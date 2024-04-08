9286bef99f3478738445e51bedb46ce63ab454a5
Yoonyoung Heo <gjdbsdud101@naver.com>
2024-04-08 16:06:01 +0900

    [Feat] Pagination 구현

--- frontend-10ea-project/c1-pt4-next-map/src/pages/api/stores.ts
+++ frontend-10ea-project/c1-pt4-next-map/src/pages/api/stores.ts
@@ -1,14 +1,28 @@
 import type { NextApiRequest, NextApiResponse } from "next";
-import { StoreType } from "@/interface";
+import { StoreType, StoreApiResponse } from "@/interface";
 import { PrismaClient } from "@prisma/client";
 
 export default async function handler(
   req: NextApiRequest,
-  res: NextApiResponse<StoreType[]>
+  res: NextApiResponse<StoreApiResponse>
 ) {
+  const { page = "1" }: { page?: string } = req.query;
   const prisma = new PrismaClient();
+
+  const count = await prisma.store.count();
+  const skipPage = parseInt(page) - 1;
   const stores = await prisma.store.findMany({
     orderBy: { id: "asc" },
+    take: 10,
+    skip: skipPage
   });
-  res.status(200).json(stores);
+
+  // totalPage, data, page
+
+  res.status(200).json({
+    page: parseInt(page),
+    data: stores,
+    totalCount: count,
+    totalPage: Math.ceil(count / 10)
+  });
 }
--- frontend-10ea-project/c1-pt4-next-map/src/pages/stores/index.tsx
+++ frontend-10ea-project/c1-pt4-next-map/src/pages/stores/index.tsx
@@ -1,17 +1,23 @@
-import { StoreType } from "@/interface";
+import { StoreApiResponse, StoreType } from "@/interface";
 import Image from "next/image";
 import axios from "axios";
 import { useQuery } from "react-query";
 import Loading from "@/components/Loading";
+import { useRouter } from "next/router";
+import Link from "next/link";
+import Pagination from "@/components/Pagination";
 
 export default function StoreListPage() {
+  const router = useRouter();
+  const { page = "1" }: { page?: any } = router.query;
+
   const {
     isLoading,
     isError,
     data: stores,
-  } = useQuery("stores", async () => {
-    const { data } = await axios("/api/stores");
-    return data as StoreType[];
+  } = useQuery(`stores-${page}`, async () => {
+    const { data } = await axios(`/api/stores?page=${page}`);
+    return data as StoreApiResponse;
   });
 
   if (isError) {
@@ -25,42 +31,46 @@ export default function StoreListPage() {
   return (
     <div className="px-4 md:max-w-5xl mx-auto py-8">
       <ul role="list" className="divide-y divide-gray-100">
-        {isLoading
-          ? <Loading />
-          : stores?.map((store, index) => (
-              <li className="flex justify-between gap-x-6 py-5" key={index}>
-                <div className="flex gap-x-4">
-                  <Image
-                    src={
-                      store?.category
-                        ? `/images/markers/${store?.category}.png`
-                        : "/images/markers/default.png"
-                    }
-                    width={48}
-                    height={48}
-                    alt="아이콘 이미지"
-                  />
-                  <div>
-                    <div className="text-sm font-semibold leading-6 text-gray-900">
-                      {store?.name}
-                    </div>
-                    <div className="mt-1 text-xs font-semibold leading-5 text-gray-500">
-                      {store?.storeType}
-                    </div>
-                  </div>
-                </div>
-                <div className="hidden sm:flex sm:flex-col sm:items:end">
+        {isLoading ? (
+          <Loading />
+        ) : (
+          stores?.data?.map((store, index) => (
+            <li className="flex justify-between gap-x-6 py-5" key={index}>
+              <div className="flex gap-x-4">
+                <Image
+                  src={
+                    store?.category
+                      ? `/images/markers/${store?.category}.png`
+                      : "/images/markers/default.png"
+                  }
+                  width={48}
+                  height={48}
+                  alt="아이콘 이미지"
+                />
+                <div>
                   <div className="text-sm font-semibold leading-6 text-gray-900">
-                    {store?.address}
+                    {store?.name}
                   </div>
                   <div className="mt-1 text-xs font-semibold leading-5 text-gray-500">
-                    {store?.phone} | {store?.foodCertifyName} |{" "}
-                    {store?.category}
+                    {store?.storeType}
                   </div>
                 </div>
-              </li>
-            ))}
+              </div>
+              <div className="hidden sm:flex sm:flex-col sm:items:end">
+                <div className="text-sm font-semibold leading-6 text-gray-900">
+                  {store?.address}
+                </div>
+                <div className="mt-1 text-xs font-semibold leading-5 text-gray-500">
+                  {store?.phone} | {store?.foodCertifyName} | {store?.category}
+                </div>
+              </div>
+            </li>
+          ))
+        )}
       </ul>
+      {stores?.totalPage && (
+        <Pagination total={stores?.totalPage} page={page} />
+      )}
     </div>
   );
 }
