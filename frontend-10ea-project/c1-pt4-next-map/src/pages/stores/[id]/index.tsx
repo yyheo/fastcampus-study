6ea5630796698ae6cd1d7dbd7821d78d579f5d23
Yoonyoung Heo <gjdbsdud101@naver.com>
2024-04-19 16:47:17 +0900

    [Feat] tailwindcss 활용해서 상세페이지 제작 - tailwindui 템플릿 활용

--- frontend-10ea-project/c1-pt4-next-map/src/pages/stores/[id]/index.tsx
+++ frontend-10ea-project/c1-pt4-next-map/src/pages/stores/[id]/index.tsx
@@ -2,6 +2,7 @@ import { useRouter } from "next/router";
 import { useQuery } from "react-query";
 import axios from "axios";
 import { StoreType } from "@/interface";
+import Loader from "@/components/Loader";
 
 export default function StorePage() {
   const router = useRouter();
@@ -17,10 +18,89 @@ export default function StorePage() {
   } = useQuery(`store-${id}`, fetchStore, {
     enabled: !!id, // id가 있는 경우에만 query를 날림. false일 경우는 query를 호출하지 않음.
   });
-  
+
+  if (isError) {
+    return (
+      <div className="w-full h-screen mx-auto pt-[30%] text-red-500 text-center font-semibold">
+        다시 시도해주세요
+      </div>
+    );
+  }
+
+  if (isFetching) {
+    return <Loader className="mt-[20%]" />;
+  }
+
   return (
-    <div>
-      <h1>Store Detail: {id}</h1>
+    <div className="max-w-5xl mx-auto px-4 py-8">
+      <div className="px-4 sm:px-0">
+        <h3 className="text-base font-semibold leading-7 text-gray-900">
+          {store?.name}
+        </h3>
+        <p className="mt-1 max-w-2xl text-sm leading-6 text-gray-500">
+          {store?.address}
+        </p>
+      </div>
+      <div className="mt-6 border-t border-gray-100">
+        <dl className="divide-y divide-gray-100">
+          <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+            <dt className="text-sm font-medium leading-6 text-gray-900">
+              카테고리
+            </dt>
+            <dd className="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">
+              {store?.category}
+            </dd>
+          </div>
+          <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+            <dt className="text-sm font-medium leading-6 text-gray-900">
+              주소
+            </dt>
+            <dd className="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">
+              {store?.address}
+            </dd>
+          </div>
+          <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+            <dt className="text-sm font-medium leading-6 text-gray-900">
+              위도
+            </dt>
+            <dd className="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">
+              {store?.lat}
+            </dd>
+          </div>
+          <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+            <dt className="text-sm font-medium leading-6 text-gray-900">
+              경도
+            </dt>
+            <dd className="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">
+              {store?.lng}
+            </dd>
+          </div>
+          <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+            <dt className="text-sm font-medium leading-6 text-gray-900">
+              연락처
+            </dt>
+            <dd className="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">
+              {store?.phone}
+            </dd>
+          </div>
+          <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+            <dt className="text-sm font-medium leading-6 text-gray-900">
+              식품 인증 구분
+            </dt>
+            <dd className="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">
+              {store?.foodCertifyName}
+            </dd>
+          </div>
+          <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+            <dt className="text-sm font-medium leading-6 text-gray-900">
+              업종명
+            </dt>
+            <dd className="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">
+              {store?.storeType}
+            </dd>
+          </div>
+        </dl>
+      </div>
     </div>
   );
 }
