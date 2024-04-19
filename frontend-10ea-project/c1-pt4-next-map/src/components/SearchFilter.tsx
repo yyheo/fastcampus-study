52fd81c86a688d71e6084c0301c24078a4b23195
Yoonyoung Heo <gjdbsdud101@naver.com>
2024-04-19 17:38:03 +0900

    [Feat] Filter 기능을 위한 지역 data import

--- frontend-10ea-project/c1-pt4-next-map/src/components/SearchFilter.tsx
+++ frontend-10ea-project/c1-pt4-next-map/src/components/SearchFilter.tsx
@@ -1,9 +1,10 @@
 import React from "react";
 import { AiOutlineSearch } from "react-icons/ai";
+import { DISTRICT_ARR } from "@/data/store";
 
 export default function SearchFilter() {
   return (
-    <div className="md:flex gap-2 my-4">
+    <div className="flex flex-col md:flex-row gap-2 my-4">
       <div className="flex items-center justify-center w-full gap-2">
         <AiOutlineSearch className="w-6 h-6" />
         <input
@@ -14,6 +15,11 @@ export default function SearchFilter() {
       </div>
       <select className="bg-gray-50 border text-gray-800 text-sm md:max-w-[200px] rounded-lg focus:border-blue-500 outline-none block w-full p-3">
         <option>지역 선택</option>
+        {DISTRICT_ARR.map((data) => (
+          <option value={data} key={data}>
+            {data}
+          </option>
+        ))}
       </select>
     </div>
   );
