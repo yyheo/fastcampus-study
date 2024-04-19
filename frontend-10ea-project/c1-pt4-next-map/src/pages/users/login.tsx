e34e1bfa22d43398598e5eb4d7a15533af714348
Yoonyoung Heo <gjdbsdud101@naver.com>
2024-04-19 15:44:41 +0900

    [Feat] 버튼으로 로그인 버튼 구현 - 로그인 후에 메인 페이지로 redirect

--- frontend-10ea-project/c1-pt4-next-map/src/pages/users/login.tsx
+++ frontend-10ea-project/c1-pt4-next-map/src/pages/users/login.tsx
@@ -1,8 +1,20 @@
+import { signIn, useSession } from "next-auth/react";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
 import { AiOutlineGoogle } from "react-icons/ai";
 import { RiKakaoTalkFill } from "react-icons/ri";
 import { SiKakao, SiNaver } from "react-icons/si";
 
 export default function LoginPage() {
+  const { status, data: session } = useSession();
+  const router = useRouter();
+
+  useEffect(() => {
+    if (status === "authenticated") {
+      router.replace("/");
+    }
+  }, [router, status]);
+  
   return (
     <div className="flex flex-col justify-center px-6 lg:px-8 h-[60vh]">
       <div className="mx-auto w-full max-w-sm">
@@ -21,22 +33,23 @@ export default function LoginPage() {
           <button
             type="button"
             className="text-white flex gap-4 bg-[#4285F4] hover:bg-[#4285F4]/90 font-medium rounded-lg w-full px-5 py-4 text-center items-center justify-center"
+            onClick={() => signIn("google")}
           >
-            <AiOutlineGoogle className="w-6 h-6"/>
+            <AiOutlineGoogle className="w-6 h-6" />
             Sign in with Google
           </button>
           <button
             type="button"
             className="text-white flex gap-5 bg-[#2db400] hover:bg-[#2db400]/90 font-medium rounded-lg w-full px-5 py-4 text-center items-center justify-center"
           >
-            <SiNaver className="w-4 h-4"/>
+            <SiNaver className="w-4 h-4" />
             Sign in with Naver
           </button>
           <button
             type="button"
             className="text-black flex gap-4 bg-[#fef01b] hover:bg-[#fef01b]/90 font-medium rounded-lg w-full px-5 py-4 text-center items-center justify-center"
           >
-            <RiKakaoTalkFill className="w-6 h-6"/>
+            <RiKakaoTalkFill className="w-6 h-6" />
             Sign in with Kakao
           </button>
         </div>
