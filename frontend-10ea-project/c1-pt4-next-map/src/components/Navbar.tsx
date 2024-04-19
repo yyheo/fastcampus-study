23e8f274090a2207a640a9ca383d8e4335c031ed
Yoonyoung Heo <gjdbsdud101@naver.com>
2024-04-19 15:27:38 +0900

    [Feat] useSession 사용해서 login, logout

--- frontend-10ea-project/c1-pt4-next-map/src/components/Navbar.tsx
+++ frontend-10ea-project/c1-pt4-next-map/src/components/Navbar.tsx
@@ -2,9 +2,11 @@ import Link from "next/link";
 import { useState } from "react";
 import { BiMenu } from "react-icons/bi";
 import { AiOutlineClose } from "react-icons/ai";
+import { useSession, signOut } from "next-auth/react"; // useSession 훅으로 세션 관리
 
 export default function Navbar() {
   const [isOpen, setIsOpen] = useState(false);
+  const { data, status } = useSession();
   return (
     <>
       <div className="navbar">
@@ -21,9 +23,13 @@ export default function Navbar() {
           <Link href="/users/likes" className="navbar__list--item">
             찜한 가게
           </Link>
-          <Link href="/api/auth/signin" className="navbar__list--item">
-            로그인
-          </Link>
+          {status === "authenticated" ? (
+            <button type="button" onClick={() => signOut()}>로그아웃</button>
+          ) : (
+            <Link href="/api/auth/signin" className="navbar__list--item">
+              로그인
+            </Link>
+          )}
         </div>
         {/* Mobile Button */}
         <div
@@ -47,7 +53,10 @@ export default function Navbar() {
             <Link href="/users/likes" className="navbar__list--item--mobile">
               찜한 가게
             </Link>
-            <Link href="/api/auth/signin" className="navbar__list--item--mobile">
+            <Link
+              href="/api/auth/signin"
+              className="navbar__list--item--mobile"
+            >
               로그인
             </Link>
           </div>
